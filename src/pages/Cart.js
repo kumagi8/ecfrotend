5b0f9aadf2f4915ef1799fe1c877e9b6b4cd11e4
kumagi8 <singhgirish0588@gmail.com>
2024-06-11 13:38:19 +0530

    quantity control

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,48 +1,76 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { removeItem, clearCart } from ".././Redux/cartSlice";
+import * as Icons from "react-bootstrap-icons";
+import { addItem } from ".././Redux/cartSlice";
 import "./cart.css";
 const Cart = () => {
   const cart = useSelector((store) => store.cart.items);
 
   const totalAmount = useSelector((state) => state.cart.totalAmount);
   const dispatch = useDispatch();
-  const [cartItems, setCartItems] = useState(null);
+  // const [cartItems, setCartItems] = useState(null);
   const removeItemHandler = (id) => {
     dispatch(removeItem(id));
-    const removeditem = cart.filter((c) => c.id !== id);
-    setCartItems(removeditem);
+    // const removeditem = cart.filter((c) => c.id !== id);
+    // setCartItems(removeditem);
   };
 
   const clearCartHandler = () => {
     dispatch(clearCart());
-    setCartItems(null);
+    // setCartItems(null);
   };
-
+  function handleAddToCart(product) {
+    dispatch(
+      addItem({
+        id: product.id,
+        title: product.title,
+        price: product.price,
+        quantity: 1,
+        totalPrice: product.price,
+      })
+    );
+  }
   useEffect(() => {
-    setCartItems(cart);
+    // setCartItems(cart);
   }, []);
 
   return (
     <div className="cart">
       <h2>Your Shopping Cart</h2>
-      {cartItems?.length === 0 ? (
+      {cart?.length === 0 ? (
         <p>Your cart is empty</p>
       ) : (
         <ul>
-          {cartItems?.map((item) => (
+          {cart?.map((item) => (
             <li key={item.id} className="cart-item">
               <div>
                 <strong>{item.title}</strong>
                 <span>Quantity: {item.quantity}</span>
                 <span>Total Price: ${item.totalPrice.toFixed(2)}</span>
               </div>
-              <button onClick={() => removeItemHandler(item.id)}>Remove</button>
+              {item.quantity === 0 ? (
+                <button onClick={() => removeItemHandler(item.id)}>
+                  Remove
+                </button>
+              ) : (
+                <div className="quantity-control">
+                  <Icons.PlusCircleFill
+                    size={20}
+                    onClick={() => handleAddToCart(item)}
+                  />
+                  <p style={{ margin: 0 }}>{item.quantity}</p>
+                  <Icons.DashCircleFill
+                    size={20}
+                    onClick={() => removeItemHandler(item.id)}
+                  />
+                </div>
+              )}
             </li>
           ))}
         </ul>
       )}
-      {cartItems?.length > 0 && (
+      {cart?.length > 0 && (
         <>
           <h3>Total Amount: ${totalAmount.toFixed(2)}</h3>
           <button onClick={clearCartHandler} className="clear-cart">
